49c166c626b1329f0866b479c19084fae5ee1ef0
mohammadkarami95 <mohammadkaramisheykhlan@gmail.com>
2020-03-08 23:51:27 +0330

    MD5 algorithim implemented

---
+++ LoginAccessLevel/Security.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginAccessLevel
+{
+    class Security
+    {
+        public static string CalculateMD5Hash(string input)
+        {
+            System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputByte = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputByte);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("x2"));
+            }
+            return sb.ToString().ToLower();
+        }
+    }
+}
